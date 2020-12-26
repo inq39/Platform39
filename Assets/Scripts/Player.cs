0aef96a06a44d049ae162a4ae0fdbac79d745512
inq39 <61730491+inq39@users.noreply.github.com>
2020-12-26 12:44:34 +0100

    added doublejump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,10 @@ public class Player : MonoBehaviour
 {
     private CharacterController _characterController;
     [SerializeField] private float _speed = 5.0f;
-    private float _gravity = 1.0f;
+    [SerializeField] private float _gravity = 0.3f;
+    [SerializeField] private float _jumpHeight = 25.0f;
+    private float _yVelocity;
+    private bool _doubleJumpAllowed = true;
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +20,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float _horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 direction = new Vector3(_horizontalInput, 0, 0);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
         Vector3 velocity = direction * _speed;
         
         if (_characterController.isGrounded)
         {
-            
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _doubleJumpAllowed = true;             
+            }
         }
         else
         {
-            velocity.y -= _gravity;
+            if (Input.GetKeyDown(KeyCode.Space) && _doubleJumpAllowed)
+            {
+                _yVelocity += _jumpHeight;
+                _doubleJumpAllowed = false;
+            }
+
+            _yVelocity -= _gravity;
         }
+
+        velocity.y = _yVelocity;
 
         _characterController.Move(velocity * Time.deltaTime);
     }
